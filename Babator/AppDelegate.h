866c3d0137f51f2e7d89613127cb3f35778adbb2
akulinsky <andrey.kulinskiy@gamil.com>
2015-06-02 13:02:17 +0300

    Initial Commit

---
+++ Babator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Babator
+//
+//  Created by Andrey Kulinskiy on 6/2/15.
+//  Copyright (c) 2015 Andrey Kulinskiy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
