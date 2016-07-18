acf428e0b61cfb8a994c27b0678a12b8cf724a43
Dan Shneider <dan.shneider@babator.com>
2016-07-18 12:27:47 +0300

    presentMoviePlayerViewControllerAnimated

--- testBabatorSDK/testBabatorSDK/ViewController.m
+++ testBabatorSDK/testBabatorSDK/ViewController.m
@@ -10,7 +10,7 @@
 #import <MediaPlayer/MPMoviePlayerViewController.h>
 #import <MediaPlayer/MPMoviePlayerController.h>
 
-#import <BabatorUI/BabatorUI.h>
+#import <BabatorUI_Lib/BabatorUI.h>
 
 @interface ViewController () <BabatorViewControllerDelegate>
 
@@ -28,24 +28,29 @@
     _mpPlayer = [[MPMoviePlayerViewController alloc] initWithContentURL:[NSURL URLWithString:@"http:\/\/mediadownload.calcalist.co.il\/Calcalist_TV\/2015\/sony_xperia_z5.mp4?source=babator"]];
     _mpPlayer.moviePlayer.shouldAutoplay = YES;
     [_mpPlayer.moviePlayer setControlStyle:MPMovieControlStyleFullscreen];
-    [self addChildViewController:self.mpPlayer];
+    
+    [self presentMoviePlayerViewControllerAnimated:_mpPlayer];
+    
     CGSize screen = self.view.frame.size;
     _playerRect = CGRectMake(0, 0, screen.width, screen.height);
     [self.view addSubview:_mpPlayer.view];
     _babtorViewController = [[BabatorViewController alloc] initWithAPIKey:@"d035223d-8bba-40d2-bb13-5a22298250c6"];
     _babtorViewController.suggestionsSize = 10;
-    [_babtorViewController setPlayer:self.mpPlayer.moviePlayer pageId:@"someid"];
+    [_babtorViewController setPlayer:self.mpPlayer pageId:@"someId"];
     _babtorViewController.delegate = self;
-    [_babtorViewController loadBabatorRecommendationsInto:self];
 
 }
 
 - (void)viewWillDisappear:(BOOL)animated {
-//    [_babtorViewController removeFromParentViewController];
+    [super viewWillDisappear:animated];
+    if (_mpPlayer.moviePlayer.fullscreen) {
+        return;
+    }
+    
+    [_babtorViewController removeBabatorRecommendations];
     _babtorViewController = nil;
     [_mpPlayer.moviePlayer stop];
     [_mpPlayer removeFromParentViewController];
-    [super viewWillDisappear:animated];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -62,12 +67,19 @@
     [coordinator animateAlongsideTransition:^(id<UIViewControllerTransitionCoordinatorContext>  _Nonnull context) {
         if (size.width < size.height) {
             _mpPlayer.view.frame = _playerRect;
-   //         _babtorViewController.view.hidden = NO;
         } else {
-    //        _babtorViewController.view.hidden = YES;
             _mpPlayer.view.frame = (CGRect){CGPointZero, size};
         }
     } completion:nil];
+}
+
+#pragma mark BabatorViewControllerDelegate
+- (void)controller:(BabatorViewController *)controller didSelectVideo:(id<BBVideoParams>)videoParams {
+    [self replaceVideoAndPlay:videoParams.url];
+}
+
+- (void)playNextSuggetion:(id<BBVideoParams>)videoParams {
+    [self replaceVideoAndPlay:videoParams.url];
 }
 
 - (void)replaceVideoAndPlay:(NSURL *)url {
@@ -81,9 +93,15 @@
     });
 }
 
-#pragma mark BabatorViewControllerDelegate
-- (void)controller:(BabatorViewController *)controller didSelectVideo:(id<BBVideoParams>)videoParams {
-    [self replaceVideoAndPlay:videoParams.url];
+- (IBAction)play:(id)sender {
+    [self.mpPlayer.moviePlayer play];
 }
 
+- (IBAction)pause:(id)sender {
+    [self.mpPlayer.moviePlayer pause];
+}
+
+- (IBAction)removeNativePlayerControls:(id)sender {
+    //    self.mpPlayer.moviePlayer.controlStyle = MPMovieControlStyleNone;
+}
 @end
