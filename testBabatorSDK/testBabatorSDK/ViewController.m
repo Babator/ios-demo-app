7d68e0551e119afc105f4afa18b0e868cb6d9c07
Dan Shneider <dan.shneider@babator.com>
2016-07-24 12:50:13 +0300

    Updated implementation

--- testBabatorSDK/testBabatorSDK/ViewController.m
+++ testBabatorSDK/testBabatorSDK/ViewController.m
@@ -29,19 +29,20 @@
     _mpPlayer = [[MPMoviePlayerViewController alloc] initWithContentURL:[NSURL URLWithString:@"http:\/\/mediadownload.calcalist.co.il\/Calcalist_TV\/2015\/sony_xperia_z5.mp4?source=babator"]];
     _mpPlayer.moviePlayer.shouldAutoplay = YES;
     [_mpPlayer.moviePlayer setControlStyle:MPMovieControlStyleFullscreen];
+    [[NSNotificationCenter defaultCenter] removeObserver:_mpPlayer
+                                                    name:MPMoviePlayerPlaybackDidFinishNotification
+                                                  object:nil];
     
     [self presentMoviePlayerViewControllerAnimated:_mpPlayer];
     
     CGSize screen = self.view.frame.size;
     _playerRect = CGRectMake(0, 0, screen.width, screen.height);
-    [self.view addSubview:_mpPlayer.view];
     _babtorViewController = [[BabatorViewController alloc] initWithAPIKey:@"d035223d-8bba-40d2-bb13-5a22298250c6"];
-
     _babtorViewController.suggestionsSize = 10;
+    [_babtorViewController setPlayer:self.mpPlayer.moviePlayer pageId:@"someId"];
     [[NSNotificationCenter defaultCenter] removeObserver:self.mpPlayer
                                                     name:MPMoviePlayerPlaybackDidFinishNotification
                                                   object:nil];
-    [_babtorViewController setPlayer:self.mpPlayer pageId:@"someId"];
     _babtorViewController.delegate = self;
 
 }
