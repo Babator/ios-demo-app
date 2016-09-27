8cc3ba9c787041dd683a5f2fcfa880aaec692117
Dan Shneider <dan.shneider@babator.com>
2016-09-27 11:32:19 +0300

    no message

---
+++ testBabatorSDK-NO_POD/testBabatorSDK/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testBabatorSDK
+//
+//  Created by Eliza Sapir on 16/05/2016.
+//  Copyright © 2016 Babator. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
