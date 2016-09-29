877b2f3030f5b4cb7f82e756c64624b31c68e4d4
Dan Shneider <dan.shneider@babator.com>
2016-09-29 09:53:02 +0300

    Latest framework

---
+++ testBabatorSDK-NO_POD/BabatorUI.framework/Headers/BabatorUI.h
@@ -0,0 +1,19 @@
+//
+//  BabatorUI.h
+//  BabatorUI
+//
+//  Created by Nissim Pardo on 08/06/2016.
+//  Copyright © 2016 babatordemo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BabatorUI.
+FOUNDATION_EXPORT double BabatorUIVersionNumber;
+
+//! Project version string for BabatorUI.
+FOUNDATION_EXPORT const unsigned char BabatorUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BabatorUI/PublicHeader.h>
+
+#import "BabatorViewController.h"
